5388ffbba4477b4ff12fe097fa2e138b5547b768
Greg Taylor <greg.taylor@aclima.io>
2016-02-08 22:47:07 -0800

    Shift the app.run() call to the dronedemo script entrypoint to avoid confusion.

--- dronedemo/main.py
+++ dronedemo/main.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
--- dronedemo/scripts/dronedemo.py
+++ dronedemo/scripts/dronedemo.py
@@ -2,4 +2,4 @@ from dronedemo.main import app
 
 
 def cli_entry():
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
